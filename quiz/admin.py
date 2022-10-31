191f60037b007f29410cb1149915bdce00ab18cb
Tomasz Rogalski <tomaszrogalski11@gmail.com>
2022-10-31 17:53:13 +0100

    T2-03 Base models: Admin register

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from quiz.models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= ['id', 'name']
+
+admin.site.register(Category, CategoryAdmin)
