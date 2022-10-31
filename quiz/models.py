c365baac321b229cb91cbfc9300b8e28e6c274d8
Tomasz Rogalski <tomaszrogalski11@gmail.com>
2022-10-31 17:35:58 +0100

    T2-01 Base: Create category model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """Category type of questions"""
+    name = models.CharField(max_length = 24)
+
+    def __str__(self):
+        return self.name
--- quiz/tests/test_models.py
+++ quiz/tests/test_models.py
@@ -1,2 +1,15 @@
-class TestModels():
-    pass
+from quiz.models import Category
+from mixer.backend.django import mixer
+import pytest
+
+
+@pytest.mark.django_db
+class TestModels():   
+    
+    def test_category_str(self):
+        category = Category.objects.create(name='MyTestCategory')
+        assert str(category) == category.name
+
+    # def test_category_str(self):
+    #     category = mixer.blend('quiz.Category', name='MyTestCategory')
+    #     assert str(category) == category.name
