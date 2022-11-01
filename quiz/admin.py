c3d72415a22480fef3a242c15088004f77ab18bb
Tomasz Rogalski <tomaszrogalski11@gmail.com>
2022-11-01 13:32:54 +0100

    T3-05 Question: template

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from quiz.models import Category
+from quiz.models import Category, Question
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display= ['id', 'name']
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display= ['id', 'category', 'true_answer']
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Question, QuestionAdmin)
--- quiz/tests/test_urls.py
+++ quiz/tests/test_urls.py
@@ -7,5 +7,5 @@ class TestUrls():
         assert resolve(path).view_name == 'quiz:home'
 
     def test_question_url(self):
-        path = reverse('quiz:question', kwargs = {'question': 1})
+        path = reverse('quiz:question', kwargs = {'question_id': 1})
         assert resolve(path).view_name == 'quiz:question'
