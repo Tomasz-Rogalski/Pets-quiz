051f5b3fee8105e7fa664a3b0185f9a5553be6b8
Tomasz Rogalski <tomaszrogalski11@gmail.com>
2022-11-01 14:00:43 +0100

    T4-01 Game:Create game model

--- quiz/models.py
+++ quiz/models.py
@@ -25,3 +25,32 @@ class Question(models.Model):
     false_answer1 = models.CharField(max_length=64)
     false_answer2 = models.CharField(max_length=64)
     false_answer3 = models.CharField(max_length=64)
+
+class Game(models.Model):
+    'Model with game statistics and options'
+    category = models.ForeignKey(Category, on_delete= models.PROTECT)
+    pet = models.ForeignKey(Pet, on_delete= models.PROTECT)
+
+    questionset = []
+
+    game_score = 0
+    current_question_nr = 0
+    game_questions_nr = 3
+    is_finished =models.BooleanField(defalt=False)
+
+
+    def __init__(self):
+        "Create and shuffle questionset"
+        self.questionset = list(Question.objects.filter(category=self.category))
+        shuffle(self.questionset)
+        self.questionset = self.questionset[:3]
+
+    def get_new_question(self):
+        self.current_question_nr += 1
+        for question in self.questionset:            
+            yield question
+
+    
+
+
+
