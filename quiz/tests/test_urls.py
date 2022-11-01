d36dafc0b4d00e76fd3127e227233e30654dfd01
Tomasz Rogalski <tomaszrogalski11@gmail.com>
2022-11-01 12:51:15 +0100

    T3-03 Question: URL

--- quiz/tests/test_urls.py
+++ quiz/tests/test_urls.py
@@ -5,3 +5,7 @@ class TestUrls():
     def test_home_url(self):
         path = reverse('quiz:home')
         assert resolve(path).view_name == 'quiz:home'
+
+    def test_question_url(self):
+        path = reverse('quiz:question', kwargs = {'question': 1})
+        assert resolve(path).view_name == 'quiz:question'
--- quiz/urls.py
+++ quiz/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name='quiz'
 
 urlpatterns = [
-    path('home', views.home, name='home')
+    path('home', views.home, name='home'),
+    path('question/<int:question_id>/', views.question, name='question'),
 ]
