81f4bd9470f99c4592a7ab30eb1cc6fb4f3ea330
Tomasz Rogalski <tomaszrogalski11@gmail.com>
2022-11-07 21:15:44 +0100

    T8-01 Frontend: Templatetags

---
+++ quiz/templatetags/randomimage.py
@@ -0,0 +1,10 @@
+import random
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def random_image():
+    images_paths = ['dogcat1.png','dogcat2.png','dogcat3.png']
+    return random.choice(images_paths)
